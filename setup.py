34944de6f6eeda4b4b7e1d755ea456ecac58e183
Anna Calinawan <anna.pamela@gmail.com>
2019-06-18 12:18:02 -0400

    starts setup file for pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cptacdream",
+    version="0.0.1",
+    author="Anna Calinawan",
+    author_email="anna.calinawan@mssm.edu",
+    description="A package from the NCI-CPTAC DREAM Proteogenomics Challenge",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+    ],
+)
