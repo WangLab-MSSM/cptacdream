0ba346647e259b020634d494151cf2b2c0a6e79e
Anna Calinawan <anna.pamela@gmail.com>
2019-06-18 12:46:50 -0400

    adds test, runs expected failure

---
+++ tests/test_cptacdream.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestCptacDream(unittest.TestCase):
+    # hello world
+    def test_hello_world_is_str(self):
+        self.assertIsInstance('hello world', int)
+
+
+if __name__ == '__main__':
+    unittest.main()
