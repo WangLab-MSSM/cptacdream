fd369536aae1819b914d174fc3f7da730e248c16
Anna Calinawan <anna.pamela@gmail.com>
2019-06-18 12:50:31 -0400

    passes sanity check

--- tests/test_cptacdream.py
+++ tests/test_cptacdream.py
@@ -4,7 +4,7 @@ import unittest
 class TestCptacDream(unittest.TestCase):
     # hello world
     def test_hello_world_is_str(self):
-        self.assertIsInstance('hello world', int)
+        self.assertIsInstance('hello world', str)
 
 
 if __name__ == '__main__':
