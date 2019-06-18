fceeb0a70297a63560b0c9a14b9ed339611a1900
Anna Calinawan <anna.pamela@gmail.com>
2019-06-18 12:18:50 -0400

    adds lab github page to setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A package from the NCI-CPTAC DREAM Proteogenomics Challenge",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/WangLab-MSSM/cptacdream",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
